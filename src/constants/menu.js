570775153e90a5c45441b96ea88706293fb3eb45
Kirill Medvedev <repkomail@gmail.com>
2025-10-19 16:33:46 +0200

    feat: add new menu

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -1,63 +1,33 @@
-// Главное меню (обычная клавиатура)
 export const mainMenu = {
   reply_markup: {
-    keyboard: [
-      ["Услуги"],
-      ['Кнопка 2'],
-      ['Кнопка 3']
-    ],
+    keyboard: [["Услуги"]],
     resize_keyboard: true,
-    one_time_keyboard: false
-  }
+    one_time_keyboard: true,
+  },
 };
-
 
 export const servicesMenu = {
   reply_markup: {
     inline_keyboard: [
+      [{ text: "Подача уведомления в РКН", callback_data: "service_1" }],
+      [{ text: "Пакет документов для психолога", callback_data: "service_2" }],
       [
         {
-          text: "Подача уведомления в РКН",
-          callback_data: "service_1"
-        }
+          text: "Вебинар «Реклама по новым правлам»",
+          callback_data: "service_3",
+        },
       ],
-      [
-        {
-          text: "Пакет документов для психологов",
-          callback_data: "service_2"
-        }
-      ],
-      [
-        {
-          text: "Реклама по новым правилам",
-          callback_data: "service_3"
-        }
-      ],
-      [
-        {
-          text: "🔙 Назад",
-          callback_data: "back_to_main"
-        }
-      ]
-    ]
-  }
+      [{ text: "← Назад", callback_data: "back_to_main" }],
+    ],
+  },
 };
 
 export const paymentMenu = {
   reply_markup: {
     inline_keyboard: [
-      [
-        {
-          text: "💳 Перейти к оформлению",
-          callback_data: "make_payment"
-        }
-      ],
-      [
-        {
-          text: "🔙 К услугам",
-          callback_data: "back_to_services"
-        }
-      ]
-    ]
-  }
+      [{ text: "💳 Перейти к оформлению", callback_data: "make_payment" }],
+      [{ text: "← К услугам", callback_data: "back_to_services" }],
+      [{ text: "🏠 Главное меню", callback_data: "back_to_main" }],
+    ],
+  },
 };
